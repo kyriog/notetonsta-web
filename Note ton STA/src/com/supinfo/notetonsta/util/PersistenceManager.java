285c626d27bbff2cc85d59d53886fe1f22c630e8
Cyril Glapa <cyril.glapa@keuse.fr>
2011-12-25 20:06:13 +0100

    Création de la classe de gestion de l'EntityManagerFactory

---
+++ Note ton STA/src/com/supinfo/notetonsta/util/PersistenceManager.java
@@ -0,0 +1,23 @@
+package com.supinfo.notetonsta.util;
+
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class PersistenceManager {
+	private static EntityManagerFactory emf;
+	
+	// To prevent instantiation
+	private PersistenceManager() {}
+	
+	public static EntityManagerFactory getEntityManagerFactory() {
+		if(emf == null){
+			emf = Persistence.createEntityManagerFactory("NoteTonSTA-PU");
+		}
+		return emf;
+	}
+	
+	public static void closeEntityManagerFactory() {
+		if(emf != null && emf.isOpen())
+			emf.close();
+	}
+}
