0bd132b3f5411ce4f99e860d593dee11dbcfa600
Cyril Glapa <cyril.glapa@keuse.fr>
2011-12-26 16:45:45 +0100

    Ajout de la vérification d'authentification pour /speaker/*

---
+++ Note ton STA/src/com/supinfo/notetonsta/filter/SpeakerFilter.java
@@ -0,0 +1,43 @@
+package com.supinfo.notetonsta.filter;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebFilter("/speaker/*")
+public class SpeakerFilter implements Filter {
+	private FilterConfig config;
+
+	@Override
+	public void init(FilterConfig config) throws ServletException {
+		this.config = config;
+	}
+
+	@Override
+	public void doFilter(ServletRequest req, ServletResponse resp,
+			FilterChain chain) throws IOException, ServletException {
+		if(req instanceof HttpServletRequest && resp instanceof HttpServletResponse) {
+			HttpServletRequest httpServletRequest = (HttpServletRequest) req;
+			HttpServletResponse httpServletResponse = (HttpServletResponse) resp;
+			
+			if(httpServletRequest.getSession().getAttribute("userId") == null) {
+				httpServletResponse.sendRedirect(config.getServletContext().getContextPath() + "/login");
+				return;
+			}
+		}
+		chain.doFilter(req, resp);
+	}
+	
+	@Override
+	public void destroy() {
+		this.config = null;
+	}
+}
