4a8dffb3c0e9ad41516b88be086078b80d7fc6e6
Cyril Glapa <cyril.glapa@keuse.fr>
2011-12-31 22:21:10 +0100

    Suppression d'imports inutiles

--- Note ton STA/src/com/supinfo/notetonsta/servlet/EditInterventionSpeakerServlet.java
+++ Note ton STA/src/com/supinfo/notetonsta/servlet/EditInterventionSpeakerServlet.java
@@ -1,10 +1,8 @@
 package com.supinfo.notetonsta.servlet;
 
 import java.io.IOException;
-import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 
