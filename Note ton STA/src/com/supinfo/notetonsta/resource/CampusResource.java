4135855010369338ccc6bc8fd8fdbf47a9e0b311
Cyril Glapa <cyril.glapa@keuse.fr>
2012-04-03 14:57:24 +0200

    Adding resource to retrieve interventions on a campus

--- Note ton STA/src/com/supinfo/notetonsta/resource/CampusResource.java
+++ Note ton STA/src/com/supinfo/notetonsta/resource/CampusResource.java
@@ -8,6 +8,8 @@ import javax.ws.rs.core.MediaType;
 import com.supinfo.notetonsta.DAO.CampusDAO;
 import com.supinfo.notetonsta.DAO.DAOFactory;
 import com.supinfo.notetonsta.entity.Campus;
+import com.supinfo.notetonsta.entity.Intervention;
+import com.supinfo.notetonsta.entity.xml.InterventionListXml;
 
 @Path("/campus")
 public class CampusResource {
@@ -29,5 +31,24 @@ public class CampusResource {
 	@GET @Produces(MediaType.APPLICATION_XML)
 	public List<Campus> getAllCampusesXML() {
 		return getAllCampuses();
+	}
+	
+	private InterventionListXml getInterventionsByCampusId(Long id) {
+		InterventionListXml interventionsList = new InterventionListXml();
+		Campus campus = campusManager.getCampusById(id);
+		for(Intervention i : campus.listInterventions()) {
+			interventionsList.addIntervention(i);
+		}
+		return interventionsList;
+	}
+	
+	@GET @Path("/{id}") @Produces(MediaType.APPLICATION_JSON)
+	public InterventionListXml getInterventionsByCampusIdJSON(@PathParam("id") Long id) {
+		return getInterventionsByCampusId(id);
+	}
+	
+	@GET @Path("/{id}") @Produces(MediaType.APPLICATION_XML)
+	public InterventionListXml getInterventionsByCampusIdXML(@PathParam("id") Long id) {
+		return getInterventionsByCampusId(id);
 	}
 }
